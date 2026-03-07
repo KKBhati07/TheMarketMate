425e058de6c4e4cd50f8578ddc1cca88751a419b
Kanishk Bhati <kk.bhatii.007@gmail.com>
2026-03-07 14:07:07 +0530

    Updated prod Url to allow API call

--- apps/marketmate/server.ts
+++ apps/marketmate/server.ts
@@ -36,7 +36,7 @@ export function app(): express.Express {
 						connectSrc: [
 							"'self'",
 							"https://api.marketmate.local:8080", // allow API calls
-							"https://springmate.onrender.com"
+							"https://api.marketmatecloud.in"
 						],
 						scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
 						scriptSrcAttr: ["'unsafe-inline'"],
