e7593bfd1a8253e64740c1914bcb598ac372fd16
Kanishk Bhati <kk.bhatii.007@gmail.com>
2026-03-08 22:26:41 +0530

    Resolved circular dependecy issue

--- libs/shared/src/lib/components/ui/app-heading/heading.component.ts
+++ libs/shared/src/lib/components/ui/app-heading/heading.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { SHARED_UI_DEPS } from '@marketmate/shared';
+import { SHARED_UI_DEPS } from '../../../constants/shared-imports';
 
 @Component({
 	selector: 'mm-heading',
